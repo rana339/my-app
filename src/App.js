8a1026db269c3df70b1fd20bdc26fc954c9fe1d3
MUHAMMADARSALANBHAI <81521989+MUHAMMADARSALANBHAI@users.noreply.github.com>
2021-12-10 22:17:04 +0500

    ffff

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+
+import Login from "./components/Login";
+import SignUp from "./components/Signup";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          <Link className="navbar-brand" to={"/sign-in"}>FRIENDS </Link>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+                <Link className="nav-link" to={"/signup"}>Sign up</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/login"}>Login</Link>
+              </li>
+              
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <Routes>
+            <Route path='/' element={<sign-in />} />
+            <Route path='/signup' element={<SignUp />} />
+            <Route path='/login' element={<Login />} />
+          
+          </Routes>
+        </div>
+      </div>
     </div>
   );
 }
